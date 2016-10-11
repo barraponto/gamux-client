81eaad9064808e296de50c51a563bf405f7bbb42
Capi Etheriel <barraponto@gmail.com>
2016-10-10 23:56:24 -0300

    [app] initialize the app with feathers and socket.io

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('this is working');
+const io = require('socket.io-client');
+const feathers = require('feathers-client');
+
+document.addEventListener('DOMContentLoaded', () => {
+  const socket = io('http://localhost:3030/');
+  const app = feathers()
+    .configure(feathers.hooks())
+    .configure(feathers.socketio(socket));
+
+  app.io.emit('lobby:list');
+  console.log('this is working');
+});
